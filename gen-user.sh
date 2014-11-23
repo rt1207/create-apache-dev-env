d853ba513c9b19569a3d1d590ef1eb94f23ef014
RyoTanaka <ryo_tn@volare.jp>
2014-11-24 02:46:23 +0900

    remove sudo

--- gen-user.sh
+++ gen-user.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 if id -u $1 >/dev/null 2>&1; then
-        echo "user exists"
+    echo "user exists"
 else
-        echo "user does not exist"
-	sudo useradd $2 -G wheel
+    echo "create user"
+	useradd $2 -G wheel
 	echo password | passwd $2 --stdin
 fi
 
+echo "create directories"
+mkdir /var/www/vhosts
+chmod 755 -R /var/www/vhosts
 mkdir -p /var/www/vhosts/production/$1
 mkdir -p /var/www/vhosts/staging/$1
 mkdir -p /var/www/vhosts/dev/$2/$1
