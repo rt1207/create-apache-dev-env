5323c8bd9f6422cf20b1fb9e5cdc08870d1076a2
silwyb <silwyb@gmail.com>
2015-01-14 21:43:36 +0900

    Update gen-user.sh

--- gen-user.sh
+++ gen-user.sh
@@ -3,6 +3,7 @@
 # sh gen-user.sh USERNAME PROJECTNAME
 #
 
+# Add User if there isn't
 if id -u $1 >/dev/null 2>&1; then
     echo "user exists"
 else
@@ -11,11 +12,13 @@ else
 	echo password | passwd $1 --stdin
 fi
 
-echo "create directories"
+# Create for ssh-login
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 touch ~/.ssh/authorized_keys
 
+# Create Directories: capistrano
+echo "create directories"
 mkdir -p /var/www/vhosts
 mkdir -p /var/www/vhosts/production/current
 mkdir -p /var/www/vhosts/staging/current
